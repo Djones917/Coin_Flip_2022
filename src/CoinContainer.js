85dc76d570a1fc42e3f3d1dcb12883c0f12cc33f
Derek <nightwing917@hotmail.com>
2022-07-08 19:36:14 -0800

    adding object for coin

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 class CoinContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currCoin: null,
+      nFlips: 0,
+      nHeads: 0,
+      nTails: 0
+    }
   }
 } 
 
