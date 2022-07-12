9544bffc51ed5dede310af3742901d073553b23a
Derek <nightwing917@hotmail.com>
2022-07-11 20:21:34 -0800

    added default props

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class CoinContainer extends Component {
+  static defaultProps = {
+    coins: [
+      {sides: 'heads', imgSrc: "https://tinyurl.com/react-coin-heads-jpg"},
+      {sides: 'tails', imgSrc: "https://tinyurl.com/react-coin-tails-jpg"}
+    ]
+  }
   constructor(props) {
     super(props);
     this.state = {
@@ -8,9 +14,7 @@ class CoinContainer extends Component {
       nFlips: 0,
       nHeads: 0,
       nTails: 0
-    }
-    {sides: 'heads', url: "https://tinyurl.com/react-coin-heads-jpg"}
-    {sides: 'tails', url: "https://tinyurl.com/react-coin-tails-jpg"}
+    }    
   }
 } 
 
