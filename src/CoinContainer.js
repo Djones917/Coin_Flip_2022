31cec85fd0515ebfd8a28f54880d5e7cbb4127cc
Derek <nightwing917@hotmail.com>
2022-07-12 19:46:17 -0800

    added p tag

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -20,6 +20,7 @@ class CoinContainer extends Component {
     return (
       <div className='CoinContainer'>
         <h2>Let's Flip A Coin!</h2>
+        <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails!</p>
       </div>
     )
   }
