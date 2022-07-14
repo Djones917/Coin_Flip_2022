4227c8c7e8b76ad48a46f94e71ac5c920f471fa5
Derek <nightwing917@hotmail.com>
2022-07-13 19:55:46 -0800

    importing coin container into app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import CoinContainer from './CoinContainer';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+       <CoinContainer />
     </div>
   );
 }
